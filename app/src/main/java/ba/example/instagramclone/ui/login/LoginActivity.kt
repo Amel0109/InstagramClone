b5c918d116cc9eb5667662b028bf006c2d570214
Amel0109 <58263087+Amel0109@users.noreply.github.com>
2019-12-10 11:12:15 +0100

    no message

---
+++ app/src/main/java/ba/example/instagramclone/ui/login/LoginActivity.kt
@@ -0,0 +1,13 @@
+package ba.example.instagramclone.ui.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ba.example.instagramclone.R
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
---
+++ app/src/main/java/ba/example/instagramclone/ui/register/RegisterActivity.kt
@@ -0,0 +1,13 @@
+package ba.example.instagramclone.ui.register
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ba.example.instagramclone.R
+
+class RegisterActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
